ac2d3375a12ace81af49abdf6de5c724aecbe63f
Anton Lapin <antonlapin@Antons-MacBook-Pro.local>
2020-04-21 16:21:46 +0300

    Final commit

--- mysite/urlshortener/models.py
+++ mysite/urlshortener/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+import random
+import string
 # Create your models here.
+
+def shorten(length=8):
+    letters = string.ascii_lowercase + string.ascii_uppercase + '1234567890'
+    return ''.join(random.choice(letters) for i in range(length))
+
+
+class URLManager(models.Manager):
+
+    def create_url(self, engine):
+        shortcut = shorten(8)
+        url_object = self.create(engine=engine, shortcut=shortcut)
+        return url_object
+
+
+
+class URLs(models.Model):
+    engine = models.CharField(max_length=2048, primary_key=True)
+    shortcut = models.CharField(max_length=8)
+    created = models.DateTimeField(auto_now_add=True)
+
+    objects = URLManager()
+
+    def __str__(self):
+        return f"{self.engine} => {self.shortcut}"
--- mysite/urlshortener/views.py
+++ mysite/urlshortener/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import URLs
 
 # Create your views here.
+
+def index(request, id=1):
+    url_object = None
+    shrinked_url = None
+    if request.method == 'POST':
+        engine_url = request.POST['engine'] # url that was written
+        try:
+            url_object = URLs.objects.get(pk=engine_url)
+        except URLs.DoesNotExist:
+            new_url = URLs.objects.create_url(engine=engine_url)
+            new_url.save()
+            url_object = new_url
+
+    if url_object is not None:
+        shrinked_url = 'http://127.0.0.1:8000/' + url_object.shortcut
+
+    context = {
+        'url_object': url_object,
+        'shrinked_url': shrinked_url,
+    }
+
+    return render(request, 'urlshortener/base.html', context)
+
+
+def redirection(request, shortcut=None):
+    try:
+        url = URLs.objects.filter(shortcut=shortcut)[0]
+    except URLs.DoesNotExist :
+         return HttpResponse('There is no such url')
+    except IndexError:
+        return HttpResponse('There is no such url')
+
+    return redirect(url.engine)
